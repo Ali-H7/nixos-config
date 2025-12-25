979ec6e6d92790a7851de2b3430f5e0dd7bf32e0
Ali-H7 <172230399+Ali-H7@users.noreply.github.com>
2025-12-26 01:43:00 +0300

    Add command alias for wireguard

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -9,4 +9,22 @@
     };
   };
 
+  environment.interactiveShellInit = ''
+    vpn() {
+      case "$1" in
+        -u|--up)
+          sudo systemctl start wg-quick-wg0.service
+          ;;
+        -d|--down)
+          sudo systemctl stop wg-quick-wg0.service
+          ;;
+        -s|--status)
+          systemctl status wg-quick-wg0.service
+          ;;
+        *)
+          echo "Usage: vpn [-u|--up] [-d|--down] [-s|--status]"
+          ;;
+      esac
+    }
+  '';
 }
