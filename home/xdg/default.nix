05f18916e526c9c35f72074cad5fd4638b9cfdb6
Ali-H7 <172230399+Ali-H7@users.noreply.github.com>
2026-01-10 10:04:11 +0300

    Set default file manager

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -1,11 +1,17 @@
 { pkgs, config, ... }:
 
 {
-  # Configure XDG user directories, which define standard locations for user files (e.g., Desktop, Documents).
-  xdg.userDirs = {
-    enable = true;
-    # Automatically create the user directories if they do not exist.
-    createDirectories = true;
-  };
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
 
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      };
+    };
+  };
 }
