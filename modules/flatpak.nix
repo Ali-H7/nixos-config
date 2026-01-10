1c1b8e98e0159a1bdcac954ab45f3f8aac2efdfc
Ali-H7 <172230399+Ali-H7@users.noreply.github.com>
2026-01-10 19:52:52 +0300

    Refactor flatpak module

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -2,30 +2,24 @@
 
 {
   imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
+
   services.flatpak = {
     enable = true;
+    update.onActivation = true;
+    uninstallUnmanaged = true;
+
     packages = [
       "org.jdownloader.JDownloader"
       "com.bitwarden.desktop"
       "com.github.tchx84.Flatseal"
-      "com.heroicgameslauncher.hgl"
     ];
 
-    update.onActivation = true;
-    # Optional: Uninstall flatpaks not listed in this config
-    uninstallUnmanaged = true;
-
-  };
-
-  system.activationScripts.jdownloader-java-fix = {
-    text = ''
-      ${pkgs.flatpak}/bin/flatpak override org.jdownloader.JDownloader --env=_JAVA_AWT_WM_NONREPARENTING=1
-    '';
-  };
-
-  system.activationScripts.bitwarden-wayland-fix = {
-    text = ''
-      ${pkgs.flatpak}/bin/flatpak override com.bitwarden.desktop --env=ELECTRON_OZONE_PLATFORM_HINT=wayland
-    '';
+    overrides = {
+      "org.jdownloader.JDownloader" = {
+        Environment = {
+          "_JAVA_AWT_WM_NONREPARENTING" = "1";
+        };
+      };
+    };
   };
 }
